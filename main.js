cf4c0bd165e8114a633482cb9e4a405ed8367be7
Francesco Sica <fsica171@gmail.com>
2021-04-21 09:00:15 +0200

    Fine section card

---
+++ main.js
@@ -0,0 +1,81 @@
+Vue.config.devtools = true;
+
+// single card
+Vue.component("single-card", {
+
+    props: {
+        card: {
+            type: Object,
+            required: true,
+        },
+    },
+
+    methods: {
+    },
+
+    template: `
+    <div class="card myCard">
+        <img class="card-img-top" :src="card.img">
+
+        <div class="myCard-body">
+
+            <div class="card-body">
+                <h5 class="card-title"> <strong>{{ card.title }}</strong> </h5>
+                <p class="card-text"> {{ card.description }} </p>
+                <a href="#" class="card-link">Learn more</a>
+            </div>
+        </div>
+    </div>
+    `
+})
+
+
+// Card-list super(single-card)
+Vue.component("card-list", {
+    props: {
+        cards: {
+            type: Array,
+            required: true,
+        },
+    },
+
+    template: `
+    <div id="cardList" class="col-12">
+
+        <single-card
+            v-for="(card,index) in cards"
+            :card ="card"
+            :key= "index">
+
+        </single-card>
+    </div>
+    `
+})
+
+
+// ****** ROOT *********
+var app = new Vue({
+
+    el: '#root',
+    data: {
+        cards:[
+            {
+                img:'assets\\img\\ptr-module.jpg',
+                title: 'Painter Essentials 8',
+                description:'The best affordable painting software for beginners'
+            },
+
+            {
+                img:'assets\\img\\particleshop-module.jpg',
+                title: 'Particle Shop',
+                description:'Powerful Adobe® Photoshop® brush plugin powered by Painter'
+            },
+
+            {
+                img:'assets\\img\\brush-pack-module.jpg',
+                title: 'Brush Packs',
+                description:'Diverse add-on packs to embellish photo composites, graphic designs and paintings'
+            }
+        ]
+    },
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,7 @@
 // webpack.mix.js
 let mix = require('laravel-mix');
-mix.sass('src/master.scss', 'assets/');
+
+
+mix.sass('src/master.scss', 'assets/').options({
+        processCssUrls: false
+    });
